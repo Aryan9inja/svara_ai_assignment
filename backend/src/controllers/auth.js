aca76c4a2437c21b0b84d2775b5845737aba7b31
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-24 21:33:44 +0530

    Add dashboard route and refactor auth controller to use asyncHandler

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,41 +1,22 @@
 import * as authService from '../services/auth.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
 
-export const register = async (req, res, next) => {
-  try {
-    authService.registerUser(req, res);
-  } catch (error) {
-    next(error);
-  }
-};
+export const register = asyncHandler(async (req, res) => {
+  await authService.registerUser(req, res);
+});
 
-export const login = async (req, res, next) => {
-  try {
-    authService.loginUser(req, res);
-  } catch (error) {
-    next(error);
-  }
-};
+export const login = asyncHandler(async (req, res) => {
+  await authService.loginUser(req, res);
+});
 
-export const logout = async (req, res, next) => {
-  try {
-    authService.logoutUser(req, res);
-  } catch (error) {
-    next(error);
-  }
-};
+export const logout = asyncHandler(async (req, res) => {
+  await authService.logoutUser(req, res);
+});
 
-export const refresh = async (req, res, next) => {
-  try {
-    authService.refreshAccessToken(req, res);
-  } catch (error) {
-    next(error);
-  }
-};
+export const refresh = asyncHandler(async (req, res) => {
+  await authService.refreshAccessToken(req, res);
+});
 
-export const getCurrent = async (req, res, next) => {
-  try {
-    authService.getCurrentUser(req, res);
-  } catch (error) {
-    next(error);
-  }
-};
+export const getCurrent = asyncHandler(async (req, res) => {
+  await authService.getCurrentUser(req, res);
+});
